505d0c406284bb7fdef4fad31725579609bd9b23
Nikolaos <lunaticnick@hotmail.com>
2024-11-21 14:26:22 +0000

    test

---
+++ index.js
@@ -0,0 +1,7 @@
+import dotenv from "dotenv"
+
+dotenv.config()
+
+console.log('===== ENV =====', process.env)
+console.log('===== ENV =====', secrets)
+console.log('===== ENV =====', process.env.SECRET)
